9d9b367e351d34f288a0ff4733cd76a59c81644f
Andrea Cecchi <andrea.cecchi85@gmail.com>
2020-06-12 13:28:46 +0200

    fix behaviors

--- src/redturtle/volto/tests/test_content_types.py
+++ src/redturtle/volto/tests/test_content_types.py
@@ -11,34 +11,38 @@ class TestSetup(unittest.TestCase):
 
     def setUp(self):
         """Custom shared utility setup for tests."""
-        self.portal = self.layer['portal']
+        self.portal = self.layer["portal"]
 
     def test_behaviors_enabled_for_basic_types(self):
-        portal_types = api.portal.get_tool(name='portal_types')
-        self.assertIn('volto.blocks', portal_types['Document'].behaviors)
-        self.assertIn('volto.blocks', portal_types['Event'].behaviors)
-        self.assertIn('volto.blocks', portal_types['News Item'].behaviors)
+        portal_types = api.portal.get_tool(name="portal_types")
+        self.assertIn("volto.blocks", portal_types["Document"].behaviors)
+        self.assertIn("volto.blocks", portal_types["Event"].behaviors)
+        self.assertIn("volto.blocks", portal_types["News Item"].behaviors)
+
+        self.assertNotIn("plone.richtext", portal_types["Document"].behaviors)
+        self.assertNotIn("plone.richtext", portal_types["Event"].behaviors)
+        self.assertNotIn("plone.richtext", portal_types["News Item"].behaviors)
 
     def test_collection_disabled(self):
-        portal_types = api.portal.get_tool(name='portal_types')
-        self.assertFalse(portal_types['Collection'].global_allow)
+        portal_types = api.portal.get_tool(name="portal_types")
+        self.assertFalse(portal_types["Collection"].global_allow)
 
     def test_document_folderish(self):
-        portal_types = api.portal.get_tool(name='portal_types')
+        portal_types = api.portal.get_tool(name="portal_types")
         self.assertEqual(
-            'collective.folderishtypes.dx.content.FolderishDocument',
-            portal_types['Document'].klass,
+            "collective.folderishtypes.dx.content.FolderishDocument",
+            portal_types["Document"].klass,
         )
 
     def test_news_not_folderish(self):
-        portal_types = api.portal.get_tool(name='portal_types')
+        portal_types = api.portal.get_tool(name="portal_types")
         self.assertEqual(
-            'plone.app.contenttypes.content.NewsItem',
-            portal_types['News Item'].klass,
+            "plone.app.contenttypes.content.NewsItem",
+            portal_types["News Item"].klass,
         )
 
     def test_event_not_folderish(self):
-        portal_types = api.portal.get_tool(name='portal_types')
+        portal_types = api.portal.get_tool(name="portal_types")
         self.assertEqual(
-            'plone.app.contenttypes.content.Event', portal_types['Event'].klass
+            "plone.app.contenttypes.content.Event", portal_types["Event"].klass
         )
