d725d096c4b12ac7f2b51aa1c1c1389417ee2668
Luca Bellenghi <lucabel@gmail.com>
2021-01-28 19:42:40 +0100

    customize syndication IFeed adapter to allow use collection RSS with volto

---
+++ src/redturtle/volto/syndication/adapters.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from Products.CMFPlone.browser.syndication.adapters import DexterityItem as BaseDexterityItem
+from Products.CMFPlone.interfaces.syndication import IFeed
+from plone import api
+from plone.dexterity.interfaces import IDexterityContent
+from zope.component import adapts
+
+
+class DexterityItem(BaseDexterityItem):
+    """
+    We need to customize this adapter to use in some specific case
+    plone collections and /RSS view.
+    To call a /RSS we need to call the customer site with '/api'
+    sub path. This means that result links contains '/api' and we
+    need to remove it
+    """
+    adapts(IDexterityContent, IFeed)
+
+    def __init__(self, context, feed):
+        super(DexterityItem, self).__init__(context, feed)
+        self.portal_url = api.portal.get().absolute_url()
+
+    @property
+    def link(self):
+        link = super(DexterityItem, self).link
+        return link.replace(self.portal_url + '/api', self.portal_url)
+        
+    guid = link
