a56dd9272c76cff31b89c996245c75da02c25f89
Giacomo Monari <arsenico13@hotmail.it>
2021-04-15 11:50:42 +0200

    Preparing release 1.2.4

--- CHANGES.rst
+++ CHANGES.rst
@@ -1,7 +1,7 @@
 Changelog
 =========
 
-1.2.4 (unreleased)
+1.2.4 (2021-04-15)
 ------------------
 
 - Customize translation for remoteUrl field description only for restapi call.
--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ long_description = "\n\n".join(
 
 setup(
     name="redturtle.volto",
-    version="1.2.4.dev0",
+    version="1.2.4",
     description="Helper package to setup a RedTurtle's Plone site ready to work with Volto.",
     long_description=long_description,
     # Get more from https://pypi.org/classifiers/
