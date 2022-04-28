3e57c968da34716464f236d00431df8cd628c7a9
Luca Bellenghi <lucabel@gmail.com>
2022-04-28 15:02:27 +0200

    try to improve date start presentation when perform @querystringsearch

--- src/redturtle/volto/restapi/services/querystringsearch/post.py
+++ src/redturtle/volto/restapi/services/querystringsearch/post.py
@@ -99,11 +99,7 @@ class RTQuerystringSearchPost(QuerystringSearchPost):
         if query_end:
             range = query_end.get("range", "")
             value = query_end.get("query", "")
-            if (
-                not query_start and
-                isinstance(value, list) and
-                range == "minmax"
-            ):
+            if not query_start and isinstance(value, list) and range == "minmax":
                 start = self.get_datetime_value(value[0])
                 end = self.get_datetime_value(value[1])
                 return start, end
