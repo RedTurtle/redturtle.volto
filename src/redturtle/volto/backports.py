9544f95f5b1ddfde6e86d411c7b7ff500060d581
mamico <mauro.amico@gmail.com>
2022-12-02 22:44:02 +0100

    pdb

--- src/redturtle/volto/backports.py
+++ src/redturtle/volto/backports.py
@@ -49,7 +49,6 @@ def image_scales(obj):
     """
     Indexer used to store in metadata the image scales of the object.
     """
-    import pdb; pdb.set_trace()
     adapter = queryMultiAdapter((obj, getRequest()), IImageScalesAdapter)
     if not adapter:
         # Raising an AttributeError does the right thing,
