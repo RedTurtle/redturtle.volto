534c2a780b6f08a0a42abc2e97301f18704d92c8
Mauro Amico <mauro.amico@gmail.com>
2023-03-10 11:53:41 +0100

    feat: advancedquery

--- src/redturtle/volto/tests/test_advancedsearch.py
+++ src/redturtle/volto/tests/test_advancedsearch.py
@@ -4,18 +4,15 @@ from plone.app.testing import setRoles
 from plone.app.testing import SITE_OWNER_NAME
 from plone.app.testing import SITE_OWNER_PASSWORD
 from plone.app.testing import TEST_USER_ID
-from plone.registry.interfaces import IRegistry
 from plone.restapi.testing import RelativeSession
-from Products.CMFPlone.interfaces import ISearchSchema
 from redturtle.volto.testing import REDTURTLE_VOLTO_API_FUNCTIONAL_TESTING
 from transaction import commit
-from zope.component import getUtility
 
 import unittest
 
 
 class AdvancedSearchTest(unittest.TestCase):
-    # TODO: definire un layer specifico per il test
+
     layer = REDTURTLE_VOLTO_API_FUNCTIONAL_TESTING
 
     def setUp(self):
