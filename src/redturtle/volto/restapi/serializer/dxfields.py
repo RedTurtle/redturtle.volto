f756427f3d81d597ec251a283c14dad07740b642
Andrea Cecchi <andrea.cecchi85@gmail.com>
2023-08-29 09:12:37 +0200

    also fix Link serializer

--- src/redturtle/volto/restapi/serializer/dxfields.py
+++ src/redturtle/volto/restapi/serializer/dxfields.py
@@ -3,16 +3,16 @@ from plone.app.contenttypes.interfaces import ILink
 from plone.app.contenttypes.utils import replace_link_variables_by_paths
 from plone.app.dexterity.behaviors.metadata import IPublication
 from plone.dexterity.interfaces import IDexterityContent
-from plone.outputfilters.browser.resolveuid import uuidToURL
 from plone.restapi.interfaces import IFieldSerializer
 from plone.restapi.serializer.converters import json_compatible
 from plone.restapi.serializer.dxfields import DefaultFieldSerializer
+from plone.restapi.serializer.utils import uid_to_url
 from redturtle.volto.interfaces import IRedturtleVoltoLayer
 from zope.component import adapter
-from zope.component import getMultiAdapter
 from zope.interface import implementer
 from zope.schema.interfaces import IDatetime
 from zope.schema.interfaces import ITextLine
+
 
 import re
 
@@ -26,21 +26,10 @@ class TextLineFieldSerializer(DefaultFieldSerializer):
         if self.field.getName() != "remoteUrl":
             return super(TextLineFieldSerializer, self).__call__()
         value = self.get_value()
-
         path = replace_link_variables_by_paths(context=self.context, url=value)
-        match = RESOLVEUID_RE.match(path)
-        if match:
-            uid, suffix = match.groups()
-            value = uuidToURL(uid)
-        else:
-            portal = getMultiAdapter(
-                (self.context, self.context.REQUEST), name="plone_portal_state"
-            ).portal()
-            ref_obj = portal.restrictedTraverse(path, None)
-            if ref_obj:
-                value = ref_obj.absolute_url()
+        url = uid_to_url(path)
 
-        return json_compatible(value)
+        return json_compatible(url)
 
 
 @adapter(IDatetime, IDexterityContent, IRedturtleVoltoLayer)
