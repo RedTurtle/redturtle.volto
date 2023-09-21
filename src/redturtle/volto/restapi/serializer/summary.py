ce47e43fa6e9136c770952007a00b856550fe84b
Andrea Cecchi <andrea.cecchi85@gmail.com>
2023-09-21 15:26:24 +0200

    Remove custom image infos in summary serializer. Now use plone.restapi base feature

--- src/redturtle/volto/restapi/serializer/summary.py
+++ src/redturtle/volto/restapi/serializer/summary.py
@@ -45,32 +45,6 @@ class DefaultJSONSummarySerializer(BaseSerializer):
         metadata_fields = query.get("metadata_fields", [])
         return "_all" in metadata_fields or self.force_all_metadata
 
-    def get_image_scales(self, data):
-        """
-        this is a backward compatibility for old volto templates that need
-        a full image scales object
-        """
-        metadata_fields = self.metadata_fields()
-
-        if "image" not in metadata_fields and not self.show_all_metadata_fields:
-            return {}
-        if data.get("image", None):
-            # it's a fullobjects data, so we already have the infos
-            return None
-        if not data.get("image_field", ""):
-            return None
-        scales = {}
-
-        for name, actual_width, actual_height in get_scale_infos():
-            scales[name] = {
-                "width": actual_width,
-                "height": actual_height,
-                "download": "{url}/@@images/{image_field}/{name}".format(
-                    url=data["@id"], image_field=data["image_field"], name=name
-                ),
-            }
-        return scales
-
     def get_remote_url(self):
         """
         Resolve uid and return ref absolute url
@@ -105,10 +79,6 @@ class DefaultJSONSummarySerializer(BaseSerializer):
             if v in EMPTY_STRINGS and k not in ["ExpirationDate", "EffectiveDate"]:
                 # this is a Volto compatibility
                 data[k] = None
-        scales = self.get_image_scales(data)
-
-        if scales:
-            data["image"] = {"scales": scales}
         if self.context.portal_type == "Link":
             remote_url = self.get_remote_url()
             # set twice because old templates can use both
