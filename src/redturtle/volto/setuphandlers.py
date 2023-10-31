75aa8d6cc6218df9acbb0e07643eb1cfacf263cb
Luca Bellenghi <lucabel@gmail.com>
2023-10-31 15:44:27 +0100

    refactor code

--- src/redturtle/volto/setuphandlers.py
+++ src/redturtle/volto/setuphandlers.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 from Products.CMFPlone.interfaces import INonInstallable
 from zope.interface import implementer
-from redturtle.volto.upgrades import upgrade_robots_txt
+from plone import api
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 @implementer(INonInstallable)
@@ -9,6 +12,53 @@ class HiddenProfiles(object):
     def getNonInstallableProfiles(self):
         """Hide uninstall profile from site-creation and quickinstaller."""
         return ["redturtle.volto:uninstall"]
+
+
+def upgrade_robots_txt(context):
+    robots = api.portal.get_registry_record("plone.robots_txt")
+    lines = robots.splitlines()
+
+    googlebot_user_agent = "User-Agent: Googlebot"
+    # I saw this writed also as googlebot, so:
+    normalized_google_bot_user_agent = googlebot_user_agent.lower().replace(" ", "")
+
+    useragent_string = "User-Agent: "
+    normalized_useragent_string = useragent_string.lower().replace(" ", "")
+
+    googlebot_allow_rule = "Allow: /*?expand*"
+
+    googlebot_index = -1
+    allow_rule_present = False
+    for i, line in enumerate(lines):
+        if line.lower().replace(" ", "") == normalized_google_bot_user_agent:
+            googlebot_index = i
+        elif (
+            googlebot_index != -1
+            and line.replace(" ", "").lower()  # noqa
+            == googlebot_allow_rule.replace(" ", "").lower()  # noqa
+        ):
+            allow_rule_present = True
+
+    if googlebot_index != -1 and not allow_rule_present:
+        # Trova l'indice della fine della sezione User-Agent: Googlebot
+        end_googlebot_index = googlebot_index + 1
+        while end_googlebot_index < len(lines) and not lines[
+            end_googlebot_index
+        ].lower().replace(" ", "").startswith(normalized_useragent_string):
+            end_googlebot_index += 1
+
+        # Aggiungi Allow: /*?expand* alla fine della sezione User-Agent: Googlebot
+        if lines[end_googlebot_index - 1] == "":
+            end_googlebot_index -= 1
+        lines.insert(end_googlebot_index, googlebot_allow_rule)
+
+        lines = "\n".join(lines)
+        api.portal.set_registry_record("plone.robots_txt", lines)
+        logger.info("Upgrade robots.txt with rule for googlebot")
+    else:
+        logger.info(
+            "Rule for Googlebot already present in robots.txt, no action needed"
+        )
 
 
 def post_install(context):
