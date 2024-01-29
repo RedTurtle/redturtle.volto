6814d782082d37d2415e84d65b9109a3bf40137c
Mauro Amico <mauro.amico@gmail.com>
2024-01-29 15:49:01 +0100

    fix tests (dont force timezone with mixed plone.api/restapi actions)

--- src/redturtle/volto/tests/test_publication_fields_fixes.py
+++ src/redturtle/volto/tests/test_publication_fields_fixes.py
@@ -7,11 +7,9 @@ from plone.app.testing import SITE_OWNER_NAME
 from plone.app.testing import SITE_OWNER_PASSWORD
 from plone.app.testing import TEST_USER_ID
 from plone.app.testing import setRoles
-from plone.registry.interfaces import IRegistry
 from plone.restapi.serializer.converters import json_compatible
 from plone.restapi.testing import RelativeSession
 from transaction import commit
-from zope.component import getUtility
 
 from redturtle.volto.testing import REDTURTLE_VOLTO_API_FUNCTIONAL_TESTING
 
