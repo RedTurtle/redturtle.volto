451389d810a2c704d7ceb90f91564b509b9c174e
Andrea Cecchi <andrea.cecchi85@gmail.com>
2024-10-23 10:43:39 +0200

    Refactored AdvancedQuery part in @search endpoint to be more extendable. (#119)

--- src/redturtle/volto/restapi/services/search/get.py
+++ src/redturtle/volto/restapi/services/search/get.py
@@ -24,6 +24,11 @@ try:
 except ImportError:
     HAS_ADVANCEDQUERY = False
 
+
+class AdvancedQueryParsingException(Exception):
+    pass
+
+
 # custom search handler
 
 
@@ -61,53 +66,21 @@ class SearchHandler(OriginalHandler):
                 del query["fullobjects"]
             else:
                 fullobjects = False
+            try:
+                queries = self.get_advanced_search_query(query=query)
+            except AdvancedQueryParsingException:
+                query = self.request.form.copy()
+                query = unflatten_dotted_dict(query)
+                return super(SearchHandler, self).search(query)
 
-            if "use_site_search_settings" in query:
-                del query["use_site_search_settings"]
-                query = self.filter_query(query)
-
-            self._constrain_query_by_path(query)
-            query = self._parse_query(query)
-            queries = []
-            term = query.get("SearchableText")
-            indexes_mapping = self.get_indexes_mapping()
-
-            for key, value in query.items():
-                index_type = indexes_mapping.get(key, None)
-                if index_type == "ZCTextIndex":
-                    # SearchableText, Title, Description
-                    queries.append(Eq(key, value))
-                elif index_type in ["KeywordIndex", "FieldIndex"]:
-                    if isinstance(value, str):
-                        value = [value]
-                    queries.append(In(key, value))
-                elif index_type == "ExtendedPathIndex":
-                    if isinstance(value["query"], list):
-                        queries.append(Or(*[Eq(key, p) for p in value["query"]]))
-                    else:  # list/tuple ?
-                        queries.append(Eq(key, value["query"]))
-                elif key in ("b_start", "b_size"):
-                    continue
-                elif index_type is None:
-                    # skip, non-existent index
-                    continue
-                else:
-                    logger.warning(
-                        f"Unsupported query parameter: {key} {index_type} {value}. Fall back to the standard query."
-                    )
-                    query = self.request.form.copy()
-                    query = unflatten_dotted_dict(query)
-                    return super(SearchHandler, self).search(query)
-
-            # term = query.pop("SearchableText")
             # TODO: mettere i parametri di ranking in registry
             # XXX: il default sul subject ha senso ? (probabilmente no), rivedere eventualmente anche i test
+            term = query.get("SearchableText")
             rs = RankByQueries_Sum(
                 (Eq("Subject", term), 16),
                 (Eq("Title", term), 8),
                 (Eq("Description", term), 6),
             )
-
             lazy_resultset = self.catalog.evalAdvancedQuery(
                 # Eq("SearchableText", term), (rs,), **query
                 And(*queries),
@@ -121,7 +94,43 @@ class SearchHandler(OriginalHandler):
             )
 
             return results
-        return super(SearchHandler, self).search(query)
+        return super().search(query)
+
+    def get_advanced_search_query(self, query):
+        if "use_site_search_settings" in query:
+            del query["use_site_search_settings"]
+            query = self.filter_query(query)
+
+        self._constrain_query_by_path(query)
+        query = self._parse_query(query)
+        queries = []
+        indexes_mapping = self.get_indexes_mapping()
+
+        for key, value in query.items():
+            index_type = indexes_mapping.get(key, None)
+            if index_type == "ZCTextIndex":
+                # SearchableText, Title, Description
+                queries.append(Eq(key, value))
+            elif index_type in ["KeywordIndex", "FieldIndex"]:
+                if isinstance(value, str):
+                    value = [value]
+                queries.append(In(key, value))
+            elif index_type == "ExtendedPathIndex":
+                if isinstance(value["query"], list):
+                    queries.append(Or(*[Eq(key, p) for p in value["query"]]))
+                else:  # list/tuple ?
+                    queries.append(Eq(key, value["query"]))
+            elif key in ("b_start", "b_size"):
+                continue
+            elif index_type is None:
+                # skip, non-existent index
+                continue
+            else:
+                logger.warning(
+                    f"Unsupported query parameter: {key} {index_type} {value}. Fall back to the standard query."
+                )
+                raise (AdvancedQueryParsingException)
+        return queries
 
     def _parse_query(self, query):
         """
