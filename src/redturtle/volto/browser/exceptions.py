d6c2411f522ac94b2ee48f0eb49d1e394c759ad1
Andrea Cecchi <andrea.cecchi85@gmail.com>
2025-08-01 11:05:41 +0200

    fix logic

--- src/redturtle/volto/browser/exceptions.py
+++ src/redturtle/volto/browser/exceptions.py
@@ -5,8 +5,8 @@ class RTExceptionView(ExceptionView):
 
     def __call__(self):
         matches = ["@@download", "@@images", "@@display-file"]
-
-        if any(x in self.request.environ.get("REQUEST_URI") for x in matches):
-            return self.request.response.redirect(self.request.URL)
+        if any(x in self.request.ACTUAL_URL for x in matches):
+            self.request.response.redirect(self.request.URL, status=302, lock=1)
+            return
 
         return super().__call__()
