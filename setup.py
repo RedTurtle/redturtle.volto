467aefa9e53402ecc3386bd945a1598cb5c20465
Mauro Amico <mauro.amico@gmail.com>
2025-11-03 17:21:58 +0100

    Back to development: 5.5.15

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ long_description = "\n\n".join(
 
 setup(
     name="redturtle.volto",
-    version="5.5.14",
+    version="5.5.15.dev0",
     description="Helper package to setup a RedTurtle's Plone site ready to work with Volto.",
     long_description=long_description,
     # Get more from https://pypi.org/classifiers/
